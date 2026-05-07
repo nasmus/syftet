1cd8bc5b378bcc3906dfd94e89cad2fbbc9861b3
nasmus <nasmus.shahadat@gmail.com>
2026-05-07 11:39:02 +0600

    contract us page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -18,9 +18,11 @@ class HomesController < ApplicationController
     define_method(action) { render action }
   end
 
+  def contact_us; end
+
   STUB_ACTIONS = %i[
     sitemap review clients events career about_us technologies
-    contact_us how_we_work life_at_syftet
+    how_we_work life_at_syftet
     ror_development java_application_development dot_net_development
     company_profile photo_gallery web_design_company
     privacy_and_policy contest
